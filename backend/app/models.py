31476ffca49e906c4ed2e1582a08db8899468b07
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-05-20 02:21:01 +0530

    fix: week id in lecture

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,14 +10,14 @@ from app.database import Base
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     username = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String(128), nullable=False)
 
 
 class Student(Base):
-    __tablename__ = 'students'
+    __tablename__ = 'student'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
@@ -27,14 +27,14 @@ class Student(Base):
 
 
 class Course(Base):
-    __tablename__ = 'courses'
+    __tablename__ = 'course'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String(100), nullable=False)  
     code = Column(String(20), unique=True, nullable=False)
 
 
 class Week(Base):
-    __tablename__ = 'weeks'
+    __tablename__ = 'week'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String(100), nullable=False)
     num = Column(Integer, nullable=False)
@@ -44,7 +44,7 @@ class Week(Base):
 
 
 class Lecture(Base):
-    __tablename__ = 'lectures'
+    __tablename__ = 'lecture'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String(100), nullable=False)
     num = Column(Integer, nullable=False)
@@ -54,7 +54,7 @@ class Lecture(Base):
     
     
 class StudentLecture(Base):
-    __tablename__ = 'student_lectures'
+    __tablename__ = 'student_lecture'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     student_id = Column(UUID(as_uuid=True), ForeignKey('student.id'), nullable=False)
     lecture_id = Column(UUID(as_uuid=True), ForeignKey('lecture.id'), nullable=False)
