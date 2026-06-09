2801114207e1e77779fdf2b0a6ae3c416ade542c
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-06-09 22:55:44 +0530

    feat: show user id on home page for debugging

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
+import { AuthContext } from "@/contexts/AuthContext";
+
+
 const HomeView: React.FC = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <div>
-        <h1>HomeView</h1>
+        <h1>Hi, {auth.user?.user_id}</h1>
     </div>
   )
 }
