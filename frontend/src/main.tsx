5e4a4904ca1f3bc4d03e4aa27cb1b6c837874b0f
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-06-09 21:29:35 +0530

    feat: add AuthProvider to main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router/index'; 
 import '@/index.css'
+import { AuthProvider } from '@/contexts/AuthProvider';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
